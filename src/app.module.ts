59b35a3df12e35714bed050130df96cd9cdd3b7c
Mohammed Murshid <111334790+murshidmp@users.noreply.github.com>
2023-12-21 21:29:15 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UrlModule } from './url/url.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, UrlModule, PrismaModule],
+  imports: [ServeStaticModule.forRoot({
+    rootPath: join('public', '..', 'public'),
+  }),AuthModule, UrlModule, PrismaModule],
 })
 export class AppModule {}
